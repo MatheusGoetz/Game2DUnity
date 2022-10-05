265ecbf8ea97587a252e196c4019872a12deadda
Matheus Goetz <matheusgoetz@gmail.com>
2022-10-05 16:26:22 -0300

    projeto jogo 2d

---
+++ Scripts/Player.cs
@@ -0,0 +1,204 @@
+
+using System.Diagnostics;
+
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class Player : MonoBehaviour
+
+{
+
+
+
+    public float Speed;
+
+    public float JumpForce;
+
+    public bool isJumping;
+
+    public bool doubleJump;
+
+    private Rigidbody2D rig;
+
+    private Animator anim;
+
+
+
+    // Start is called before the first frame update
+
+    void Start()
+
+    {
+
+        rig = GetComponent<Rigidbody2D>();
+
+        anim = GetComponent<Animator>();
+
+    }
+
+
+
+    // Update is called once per frame
+
+    void Update()
+
+    {
+
+        move();
+
+        Jump();
+
+    }
+
+
+
+    void move()
+
+    {
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+
+        transform.position += movement * Time.deltaTime * Speed;
+
+    
+
+        if(Input.GetAxis("Horizontal") > 0f)
+
+        {
+
+            anim.SetBool("walk", true);
+
+            transform.eulerAngles = new Vector3 (0f,0f,0f);
+
+        }
+
+
+
+        if(Input.GetAxis("Horizontal") < 0f)
+
+        {
+
+            anim.SetBool("walk", true);
+
+            transform.eulerAngles = new Vector3 (0f,180f,0f);
+
+        }
+
+
+
+        if(Input.GetAxis("Horizontal") == 0)
+
+        {
+
+            anim.SetBool("walk", false);
+
+        }
+
+        
+
+    }
+
+
+
+    void Jump()
+
+    {
+
+        if(Input.GetButtonDown("Jump"))
+
+        {
+
+            if(!isJumping)
+
+            {
+
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+
+                doubleJump = true;
+
+                anim.SetBool("jump", true);
+
+            }
+
+            else
+
+            {
+
+                if(doubleJump)
+
+                {
+
+                    rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+
+                    doubleJump = false;
+
+                }
+
+            
+
+            }
+
+            
+
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+
+    {
+
+        if(collision.gameObject.layer == 8)
+
+        {
+
+            isJumping = false;
+
+            anim.SetBool("jump", false);
+
+        }
+
+        if(collision.gameObject.tag == "Spike")
+
+        {
+        
+            GameController.instance.ShowGameOver();
+            Destroy(gameObject);
+        
+        } 
+
+         if(collision.gameObject.tag == "saw")
+
+        {
+        
+            GameController.instance.ShowGameOver();
+            Destroy(gameObject);
+        
+        } 
+
+        if(collision.gameObject.tag == "Next_lvl")
+        {
+            GameController.instance.ShowNextLevel();
+        }
+    }      
+
+    void OnCollisionExit2D(Collision2D collision)
+
+    {
+
+         if(collision.gameObject.layer == 8)
+
+        {
+
+            isJumping = true;
+
+        }
+
+    }
+    
+}
